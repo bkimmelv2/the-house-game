50cc1bca1d6f6bb840093a273400ec0a29e9e3bc
Baylee Kimmel <bkimmel1226@gmail.com>
2024-03-02 10:44:25 -0500

    created core files, started a plan

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// PLAN:
+// Create a grid using for loop. Make each element an empty div with an id of (id="grid-" + i) and append to the main grid div.
+// Add event listeners to the grid ids that I want something to happen in (clue, nonclue, joke, scary popup)
+// Place a password textbox and submit button in front of all other elements. (must only accept a certain word)
+// Add my main image and use absolute positioning to ensure the grid stays over the image.
+
+// SIDE TASKS:
+// Figure out how to make a how-to modal that pops up as the page loads. You then click the play button to make the modal disappear.
+// Figure out how to play an audio file on loop.

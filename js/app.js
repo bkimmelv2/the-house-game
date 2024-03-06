99b33f77acdb8e5f4c931492c575802be9c8baff
Baylee Kimmel <bkimmel1226@gmail.com>
2024-03-05 22:59:27 -0500

    added background image and password box/button

--- js/app.js
+++ js/app.js
@@ -10,14 +10,45 @@
 
 const mainGrid = document.getElementById('grid')
 
-for (let i = 1; i <= 16; i++) {
+for (let i = 1; i <= 64; i++) {
     const gridItem = document.createElement('div')
     gridItem.id = 'grid-' + i
     gridItem.classList.add('grid-item')
     mainGrid.appendChild(gridItem)
 }
 
-const button1 = document.getElementById('grid-1')
-button1.addEventListener('click', () => {
-    alert('you clicked me!')
+// Random number generator if I need it later:
+// const randomNumber = Math.floor(Math.random() * 64) + 1
+
+// Write a function for each different click event (i.e. clue, nonclue, scary picture/sound, etc.)
+const foundClue1 = () => {
+    // using alerts as placeholder. Will use dialog later.
+    alert('You found a clue! \n\nI speak without a mouth, heard but never seen. Outside I am hard to find, yet in a room I am serene.')
+}
+
+const foundClue2 = () => {
+    alert('You found a clue! \n\nIn solitude, I am loud, yet in crowds, I am drowned. In your deepest thoughts, I am often found.')
+}
+
+const foundFalseClue1 = () => {
+    alert('You found a book! You dust it off and open it, only to realize it\'s just a dictionary. Strange... the page is saved at the beginning of the S section.')
+}
+
+// const foundFalseClue2 = () => {
+//     alert('')
+// }
+
+// const foundPoison = () => {
+//     alert('You start to feel dizzy as you realize you\'ve just touched poison! Ouch, poor thing. Try again next time!')
+// }
+
+// const foundScare = () => {
+
+// }
+
+// We can then call the chosen function on whatever div space we pick.
+
+const findButton = document.getElementById(`grid-1`)
+findButton.addEventListener('click', () => {
+    foundClue1()
 })
